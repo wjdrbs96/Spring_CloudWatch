1785cad6c1de2545a57bfcef381c0484de18a248
wjdrbs96 <wjdrbs966@naver.com>
2021-05-13 10:47:35 +0900

    CloudWatch Error log

---
+++ src/main/java/com/example/spring_cloudwatch/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.spring_cloudwatch.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by jg 2021/05/11
+ */
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        log.error("에러입니다!");
+        return "hello";
+    }
+}
